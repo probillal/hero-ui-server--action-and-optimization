2eb429224bb647fc72da054df623e3bb7dccc5f8
probillal <smbillalhossain273@gmail.com>
2026-05-20 06:12:20 +0600

    Explore button in hero UI

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@heroui/react";
+
+const HeroPage = () => {
+  return (
+    <div>
+      <h2>Hero UI Explore</h2>
+      <Button variant="primary">Primary Button</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="ghost">Ghost</Button>
+    </div>
+  );
+};
+
+export default HeroPage;
