7fc5043e46e8cc3a46b907618dfe4d9f044d20ee
probillal <smbillalhossain273@gmail.com>
2026-05-21 05:55:39 +0600

    Navbar added with hero ui

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Link, Button } from "@heroui/react";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 z-40 w-full border-b border-separator bg-background/70 backdrop-blur-lg">
+      <header className="flex h-16 items-center justify-between px-6">
+        <div className="flex items-center gap-3">
+          {/* <Logo /> */}
+          <p className="font-bold">ACME</p>
+        </div>
+        <ul className="flex items-center gap-4">
+          <li>
+            <Link className="no-underline" href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="no-underline" href="/task">
+              Task
+            </Link>
+          </li>
+        </ul>
+      </header>
+    </nav>
+  );
+};
+
+export default Navbar;
