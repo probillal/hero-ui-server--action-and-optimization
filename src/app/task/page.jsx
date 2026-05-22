a241012f7a851081e910161ce9cbed0fecc5ff8c
probillal <smbillalhossain273@gmail.com>
2026-05-22 16:03:07 +0600

    modal added in  task component

--- src/app/task/page.jsx
+++ src/app/task/page.jsx
@@ -1,3 +1,4 @@
+import AddTask from "@/components/AddTask";
 import TaskCard from "@/components/TaskCard";
 import { getTask } from "@/lib/task";
 import React from "react";
@@ -5,9 +6,10 @@ import React from "react";
 const TaskPage = async () => {
   const task = await getTask();
   return (
-    <div>
-      <h2>Task Management: {task.length}</h2>
-      <div className="grid grid-cols-3 gap-4 container mx-auto my-5">
+    <div className="container mx-auto">
+      <h2 className="mb-6">Task Management: {task.length}</h2>
+      <AddTask></AddTask>
+      <div className="grid grid-cols-3 gap-4  my-5 pt-8">
         {task.map((item) => (
           <TaskCard key={item.id} item={item}></TaskCard>
         ))}
